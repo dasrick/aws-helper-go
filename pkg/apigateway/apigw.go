218fbbc143d24da24da6107ced4486d1a6850266
Enrico Hoffmann <dasrick@gmail.com>
2019-02-02 19:56:57 +0100

    add first pkg

---
+++ pkg/apigateway/apigw.go
@@ -0,0 +1,40 @@
+package apigateway
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type APIGatewayProxyResponseError struct {
+	Message string `json:"message"`
+}
+
+func GetAPIGatewayProxyResponse200(body interface{}) (events.APIGatewayProxyResponse, error) {
+	responseJson, _ := json.Marshal(body)
+	return events.APIGatewayProxyResponse{
+		Body:       string(responseJson),
+		StatusCode: 200,
+	}, nil
+}
+
+func GetAPIGatewayProxyResponse400() (events.APIGatewayProxyResponse, error) {
+	responseJson, _ := json.Marshal(
+		&APIGatewayProxyResponseError{
+			Message: "Bad Request",
+		})
+	return events.APIGatewayProxyResponse{
+		Body:       string(responseJson),
+		StatusCode: 400,
+	}, nil
+}
+
+func GetAPIGatewayProxyResponse500() (events.APIGatewayProxyResponse, error) {
+	responseJson, _ := json.Marshal(
+		&APIGatewayProxyResponseError{
+			Message: "Internal Server Error",
+		})
+	return events.APIGatewayProxyResponse{
+		Body:       string(responseJson),
+		StatusCode: 500,
+	}, nil
+}
