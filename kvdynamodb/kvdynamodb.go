bfb8d442bb37069f5cc2633d98688b82f8c4b91f
Enrico Hoffmann <dasrick@gmail.com>
2019-02-20 16:36:24 +0100

    hashKey and TableName are now required

--- kvdynamodb/kvdynamodb.go
+++ kvdynamodb/kvdynamodb.go
@@ -1,6 +1,7 @@
 package kvdynamodb
 
 import (
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
@@ -8,7 +9,7 @@ import (
 )
 
 type Client struct {
-	c         *dynamodb.DynamoDB
+	svc       *dynamodb.DynamoDB
 	tableName string
 	hashKey   string
 }
@@ -20,25 +21,26 @@ type Options struct {
 }
 
 var DefaultOptions = Options{
-	Region:    "eu-west-1",
-	TableName: "pleaseSetATableName",
+	Region: "eu-west-1",
 }
 
 func NewClient(options Options) (Client, error) {
 	result := Client{}
 	// check incoming values and use default as fallback
-	// ToDo maybe it is possible to get the region from context or any other env ... the the default is not needed
 	if options.Region == "" {
 		options.Region = DefaultOptions.Region
 	}
 	if options.TableName == "" {
-		options.TableName = DefaultOptions.TableName
+		return result, errors.New("missing options.TableName")
+	}
+	if options.HashKey == "" {
+		return result, errors.New("missing options.HashKey")
 	}
 	// create session
 	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(options.Region)}))
 	svc := dynamodb.New(sess)
 	// map to client
-	result.c = svc
+	result.svc = svc
 	result.tableName = options.TableName
 	result.hashKey = options.HashKey
 	// thats it
@@ -51,9 +53,7 @@ func NewClient(options Options) (Client, error) {
 //}
 
 func (c Client) Get(k string, v interface{}) (bool, error) {
-	// ToDo validation of key is missing ... both are required and MUST be NOT empty
-
-	resultOutput, err := c.c.GetItem(&dynamodb.GetItemInput{
+	resultOutput, err := c.svc.GetItem(&dynamodb.GetItemInput{
 		TableName: aws.String(c.tableName),
 		Key: map[string]*dynamodb.AttributeValue{
 			c.hashKey: {
@@ -72,14 +72,12 @@ func (c Client) Get(k string, v interface{}) (bool, error) {
 }
 
 func (c Client) Store(k string, v interface{}) error {
-	// ToDo validation of key and value is missing ... both are required and MUST be NOT empty
-
 	item, err := dynamodbattribute.MarshalMap(v)
 	if err != nil {
 		return err
 	}
 
-	_, err = c.c.PutItem(&dynamodb.PutItemInput{
+	_, err = c.svc.PutItem(&dynamodb.PutItemInput{
 		TableName: aws.String(c.tableName),
 		Item:      item,
 	})
